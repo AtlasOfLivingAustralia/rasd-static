a85e5e19c812223f21f27e0879bd08da9db5f482
Dylan Li <39111051+DylanLi-Hang@users.noreply.github.com>
2025-01-10 09:55:12 +1100

    Closes #44 Update the Pipeline to build the Quarto site on AWS (#48)
    * Update .gitignore
    
    * Add build quarto phase to pipeline
    
    * Add dynamic domain and auto update on publish date
    
    * Remove quarto output files

---
+++ cicd/static-page/pipeline/modify_env.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+###
+# Script used to modify the footer website link and publish date in the quarto source files _quarto.yaml
+# Usage: modify_env.py ../quarto-source-files/_quarto.yaml
+
+from datetime import datetime
+import yaml
+import sys
+import os
+
+
+if __name__ == '__main__':
+    args = sys.argv
+
+    if len(args) != 2:
+        print('Usage: python modify_env.py <env>')
+        sys.exit(1)
+
+    with open(args[1], 'r') as file:
+        env = yaml.safe_load(file)
+    
+    # Get environment variables
+    hosted_zone = os.getenv('HOSTED_ZONE', 'rasd.org.au')
+    sub_domain = os.getenv('SUB_DOMAIN', 'www')
+
+    # Change footer and navbar url to rasd
+    url = f"{sub_domain}.{hosted_zone}"
+    html_url = f"https://{url}"
+    env['book']['navbar']['href'] = html_url
+    env['book']['page-footer']['center'][0]['text'] = url
+    env['book']['page-footer']['center'][0]['href'] = html_url
+    
+    # Update publised date
+    current_date = datetime.now().strftime('%m/%d/%Y')
+    print(current_date)
+    env['book']['date'] = current_date
+
+    with open(args[1], 'w') as file:
+        yaml.dump(env, file, sort_keys=False)
+    
+    print(env)
+    print('Environment variables updated successfully')
