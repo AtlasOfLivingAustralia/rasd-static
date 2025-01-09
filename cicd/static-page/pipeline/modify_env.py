97d7e7cdfe68333d639050eecfba21c0347adcb6
Yaohang <hang20145@gmail.com>
2025-01-09 16:59:46 +1100

    Add auto update on publish date, remove unnecessary files

--- cicd/static-page/pipeline/modify_env.py
+++ cicd/static-page/pipeline/modify_env.py
@@ -1,6 +1,8 @@
+from datetime import datetime
 import yaml
 import sys
 import os
+
 
 if __name__ == '__main__':
     args = sys.argv
@@ -11,17 +13,25 @@ if __name__ == '__main__':
 
     with open(args[1], 'r') as file:
         env = yaml.safe_load(file)
-        print(env)
-    hosted_zone = os.getenv('HOSTED_ZONE')
-    sub_domain = os.getenv('SUB_DOMAIN')
+    
+    # Get environment variables
+    hosted_zone = os.getenv('HOSTED_ZONE', 'rasd.org.au')
+    sub_domain = os.getenv('SUB_DOMAIN', 'www')
 
+    # Change footer and navbar url to rasd
     url = f"{sub_domain}.{hosted_zone}"
     html_url = f"https://{url}"
     env['book']['navbar']['href'] = html_url
     env['book']['page-footer']['center'][0]['text'] = url
     env['book']['page-footer']['center'][0]['href'] = html_url
-    # branch_name = env['pCleanBranch']
-
+    
+    # Update publised date
+    current_date = datetime.now().strftime('%m/%d/%Y')
+    print(current_date)
+    env['book']['date'] = current_date
 
     with open(args[1], 'w') as file:
         yaml.dump(env, file, sort_keys=False)
+    
+    print(env)
+    print('Environment variables updated successfully')
